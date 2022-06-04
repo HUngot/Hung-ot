826c9539457054fdb80a2b76d52fc46679ea0f6e
HUngot <106876913+HUngot@users.noreply.github.com>
2022-06-04 21:43:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Love confess",
-    introTitle: 'Who are you',
+    introTitle: 'DANG AY',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
